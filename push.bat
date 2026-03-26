ce5d7232ee9b6f978919d6ee4de0185887146fe6
Diego Andres Florez Oliveros <diego.florezol@unipaz.edu.co>
2026-03-26 15:45:28 -0500

    feat: organize profile picture for GitHub in fotoperfil folder

--- push.bat
+++ push.bat
@@ -1,5 +1,7 @@
 @echo off
 set "PATH=C:\Program Files\Git\bin;%PATH%"
+if not exist "fotoperfil" mkdir "fotoperfil"
+copy "public\images\profile.png" "fotoperfil\profile.png"
 git add -A
-git commit -m "feat: add professional profile picture"
+git commit -m "feat: organize profile picture for GitHub in fotoperfil folder"
 git push origin main
