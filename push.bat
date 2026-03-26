c722ec50e23c98cc790a1bc095947731c15c5f39
Diego Andres Florez Oliveros <diego.florezol@unipaz.edu.co>
2026-03-26 15:11:15 -0500

    Refactor: remove 'Leer articulo' button from Publications and sync user local changes

--- push.bat
+++ push.bat
@@ -1,10 +1,5 @@
 @echo off
 set "PATH=C:\Program Files\Git\bin;%PATH%"
-
-git config user.name "Diego Andres Florez Oliveros"
-git config user.email "diego.florezol@unipaz.edu.co"
 git add -A
-git commit -m "feat: portfolio profesional con Astro, Tailwind CSS y Supabase"
-git branch -M main
-git remote add origin https://github.com/dythole-zo/miportafolio.git
-git push -u origin main
+git commit -m "Refactor: remove 'Leer articulo' button from Publications and sync user local changes"
+git push origin main
