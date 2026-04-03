b9ddad37b0c0cd2e795b96d48477cf52f85015ef
Diego Andres Florez Oliveros <diego.florezol@unipaz.edu.co>
2026-04-03 11:44:43 -0500

    feat: actualizar imagen de seccion Sobre mi

--- push.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set "PATH=C:\Program Files\Git\bin;%PATH%"
-if not exist "fotoperfil" mkdir "fotoperfil"
-copy "public\images\profile.png" "fotoperfil\profile.png"
-git add -A
-git commit -m "feat: organize profile picture for GitHub in fotoperfil folder"
-git push origin main
